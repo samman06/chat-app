113f0e6b28b3f116e385f51d7cb16db4b29ff753
Mohammed-Elsamman <monem7242@gmail.com>
2020-04-26 04:28:32 +0200

    create connect event with socket

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 let app = express();
 const path = require("path");
+const http = require("http")
+const sockitIO = require("socket.io")
 
 const publicPath = path.join(__dirname, '/public')
 app.use(express.static(publicPath));
 
 
+const server = http.createServer(app);
+let io = sockitIO(server);
+io.on("connection", (sockit) => {
+    console.log("new connection")
+})
+
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`app listen on port ${PORT}`)
 })
