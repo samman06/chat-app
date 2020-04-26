bb64be652bcff0de9d0c12de76e31b7520ad41fb
Mohammed-Elsamman <monem7242@gmail.com>
2020-04-26 22:02:13 +0200

    refactor my code and create generateMessage function

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const http = require("http")
 const sockitIO = require("socket.io")
 
+const { generateMessage } = require("./server/utils/message")
+
 const publicPath = path.join(__dirname, '/public')
 app.use(express.static(publicPath));
 
@@ -12,21 +14,10 @@ const server = http.createServer(app);
 let io = sockitIO(server);
 io.on("connection", (socket) => {
     console.log("new connection")
-    socket.emit("newMessage", {
-        from: "samman",
-        text: "welcome to our chat app",
-    })
-    socket.broadcast.emit("newMessage", {
-        from: "samman",
-        text: "new user joined us",
-        createdAt: new Date().getTime()
-    })
-    socket.on("createMessage", (message) => {
-        io.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+    socket.emit("newMessage", generateMessage("samman", "welcome to our chat app"))
+    socket.broadcast.emit("newMessage", generateMessage("samman", "new user joined us"))
+    socket.on("createMessage", ({ from, text }) => {
+        io.emit("newMessage", generateMessage(from, text))
     })
 })
 
